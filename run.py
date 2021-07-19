36b3597c75d20f9de3a57d0b84350df8f09dbd22
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-20 00:21:20 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from pyrogram import Client
-from asyncio import sleep
+from time import sleep
 api_id = 6630105; api_hash = "8508c1bcfdb0c54ccc610f29fc041d99"
 app = Client('test', api_id, api_hash)
 
@@ -7,7 +7,10 @@ with app:
 	while True:
 		try:
 			e = app.send_message('ava_anime1', '<b>Канал был захвачен.</b>\nhttps://t.me/d3ntly/34')
+			sleep(0.4)
 			app.pin_chat_message('ava_anime1', e.message_id)
+			app.send_video('ava_anime1', 'https://t.me/ava_anime1/2728')
+			sleep(0.4)
 		except:
 			pass
 app.run()
